e7902f5e71bb806fc034e3e6cdc88ea703185d2b
Vadim <leprechaunvanrosenkreuz@gmail.com>
2024-04-02 22:45:50 +0200

    init commit

---
+++ src/main/java/org/demo/Main.java
@@ -0,0 +1,9 @@
+package org.demo;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Azure Batch! | " + LocalDateTime.now());
+    }
+}
